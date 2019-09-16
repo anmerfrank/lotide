af3fc501a107005b78dd780db8ee3d7907131067
Anne Franklin <anmerfrank@gmail.com>
2019-09-16 23:40:51 +0000

    Cats and Assertions

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log("😿 😿 😿 " + "Assertion Failed: " + actual + " !== " + expected);
+  } else {
+    console.log("🐱 🐱 🐱 " + "Assertion Passed: " + actual + " === " + expected);
+  }
+
+};
+
+// TEST CODE
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+assertEqual(2.3, 6);
+assertEqual("music", "art");
