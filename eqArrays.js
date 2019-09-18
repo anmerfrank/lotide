dd3a3bdb1807709ba54872d66c9ef4dcf338a63f
Anne Franklin <anmerfrank@gmail.com>
2019-09-18 01:49:31 +0000

    Equal array checker

---
+++ eqArrays.js
@@ -0,0 +1,25 @@
+//Equal Assertion -- this already runs OK from last assignment.
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed`);
+  }
+};
+
+// Equal Array checker -- this runs fine!
+
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] === arr2[i]) {
+      return true;
+    } else return false;
+  }
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); //  pass
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); //should fail
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
