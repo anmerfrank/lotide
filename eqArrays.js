00cfebb7bcfeddb9944d8aca7692646952d7ea8a
Anne Franklin <anmerfrank@gmail.com>
2019-09-18 02:29:57 +0000

    Fixed eqArrays function as per assertArraysEqual assignment.

--- eqArrays.js
+++ eqArrays.js
@@ -11,11 +11,12 @@ const assertEqual = function(actual, expected) {
 // Equal Array checker -- this runs fine!
 
 const eqArrays = function(arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] === arr2[i]) {
-      return true;
-    } else return false;
+  for (let i = 0; i <= arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
   }
+  return true;
 };
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); //  pass
