5b818ec060bce51a4a0382c40cf6394e207a7c3c
Anne Franklin <anmerfrank@gmail.com>
2019-09-19 03:06:09 +0000

    Now runs! Thanks to Brandon for the help / pair programming.

--- eqObjects.js
+++ eqObjects.js
@@ -7,51 +7,47 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+
+// eqArrays Checker
+
+const eqArrays = function(actual, expected) {
+  for (let i = 0; i < actual.length; i++) {
+    if (actual.indexOf(expected[i]) === -1) {
+      return false;
+    }
+  }
+  return true;
+};
+
 // eqObjects Checker
 
 const eqObjects = function(object1, object2) {
   let obj1 = Object.keys(object1); // comparing length
   let obj2 = Object.keys(object2);
 
-
   if (obj1.length !== obj2.length) {
     return false;
   }
-  return true;
-    
-  // //COMPARING OBJECT LENGTHS
-
-  for (let i = 0; i < obj1.length; i++) {
-    for (let j = 0; j < obj2.length; j++) {
-      if (obj1[i] !==  obj2[j]) {
+  for (const key of obj1){
+    if (Array.isArray(obj1[key]) && Array.isArray(obj2[key])){
+      if (!eqArrays(obj1[key], obj2[key])){
         return false;
-      }
+        } 
     }
-  }
-  return true;
-
-  for (const key of obj1) {
-    if (obj1[key] !==  obj2[key]) {
+    if (obj1[key] !== obj2[key]){
       return false;
     }
+    }
+    return true;
   }
-  return true;
-};
-  
 
 
 // TEST CODE
 
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-console.log(eqObjects(ab, ba)); // => true
-
-const abc = { a: "1", b: "2", c: "3" };
-console.log(eqObjects(ab, abc)); // => false
 
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
-console.log(eqObjects(cd, dc)); // => true
+assertEqual(eqObjects(cd, dc), true); // => true
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
-console.log(eqObjects(cd, cd2));
+assertEqual(eqObjects(cd, cd2)); // => false
