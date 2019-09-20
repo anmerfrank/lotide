a50584d76249c8c16c367daba3fa947e4eb1b770
Anne Franklin <anmerfrank@gmail.com>
2019-09-20 01:16:24 +0000

    Pair programming this one with Brandon

---
+++ takeUntil.js
@@ -0,0 +1,52 @@
+// ASSERT EQUAL - COPIED FUNCTIONS
+
+//Equal Arrays function
+
+const eqArrays = function(arr1, arr2) {
+  for (let i = 0; i <= arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`🐱 🐱 🐱 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😿 😿 😿 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+/// END OF ASSERT EQUAL FUNCTIONS ///
+
+// -----
+
+// The function will return a "slice of the array with
+// elements taken from the beginning." It should keep 
+// going until the callback/predicate returns a truthy value.
+
+const takeUntil = (array, callback) => {
+  for (item in array){
+    if(callback(array[item])){
+      array.splice(item);
+    } 
+  } 
+  return array;
+
+}
+
+
+// TEST CODE
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+assertArraysEqual((results1), [1, 2, 5, 7, 2]);
+assertArraysEqual((results2), ["I've", "been", "to", "Hollywood"]);
