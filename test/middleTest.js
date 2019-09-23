78f9f84fd57b8081d0bcfe613d78184f7fb0a207
Anne Franklin <anmerfrank@gmail.com>
2019-09-23 23:05:15 +0000

    Middle - Test code, refactored

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,11 +1,11 @@
 const middle = require('../middle');
 const eqArrays = require('../eqArrays');
 const assertEqual = require('../assertEqual');
-
+const assertArraysEqual = require('../assertArraysEqual');
 
 // TEST CODE
 
 
-middle([1, 2]);
-middle([1, 2, 3, 4]); // => [2, 3]
-middle([1, 2, 3, 4, 5, 6]); // => [3, 4]
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
