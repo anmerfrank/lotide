40fdbe47f01433990d5fc46db2b3c02166086213
Anne Franklin <anmerfrank@gmail.com>
2019-09-23 22:28:27 +0000

    Middle, refactored

--- middle.js
+++ middle.js
@@ -1,24 +1,4 @@
 
-// Equal Array checker
-
-const eqArrays = function(arr1, arr2) {
-  for (let i = 0; i <= arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-// Assert Equal Array:
-
-const assertArraysEqual = function(arr3, arr4) {
-  if (eqArrays(arr3, arr4)) {
-    console.log("These arrays are equal.");
-  } else {
-    console.log("These arrays are not equal.");
-  }
-};
 
 // Returns middle of the array:
 
@@ -30,16 +10,13 @@ const middle = function(arr) {
   }
 
   if (arr.length > 2 && arr.length % 2 === 0) {
+    midIndex.push(arr[Math.floor(arr.length / 2) - 1]);
     midIndex.push(arr[Math.floor(arr.length / 2)]);
-    midIndex.push(arr[Math.floor(arr.length / 2 + 1)]);
   }
   console.log(midIndex);
   return midIndex;
   
 };
 
-middle([1, 2]);
-middle([1, 2, 3, 4]); // => [2, 3]
-middle([1, 2, 3, 4, 5, 6]); // => [3, 4]
 
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+module.exports = middle;
